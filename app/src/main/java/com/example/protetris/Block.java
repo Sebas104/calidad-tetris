12814902419adec6753dd29e61a8cf0b37ff4142
Jose Luis <jlsierrabenito@gmail.com>
2019-09-30 21:57:33 +0200

    Añadido las 7 piezas y el diseño de controls
    Además de las 7 piezas están las cuadrados que forman las piezas

---
+++ app/src/main/java/com/example/protetris/Block.java
@@ -0,0 +1,19 @@
+package com.example.protetris;
+
+import android.view.View;
+
+public class Block extends View {
+
+    protected static final int EMPTY_BLOCK = 0;
+    protected static final int RED_BLOCK = 1;
+    protected static final int BLUE_BLOCK = 2;
+    protected static final int GREEN_BLOCK = 3;
+    protected static final int PURPLE_BLOCK = 4;
+    protected static final int YELLOW_BLOCK = 5;
+    protected static final int LIGHTBLUE_BLOCK_=6;
+    protected static final int ORANGE_BOCK = 7;
+    protected static final int GRAY_BLOCK = 8;
+    protected static final int SHADOW_BLOCK = 9;
+    protected static final int WHITE_BLOCK=10;
+    protected static final int NUM_BLOCKS = 9;
+}
--- app/src/main/java/com/example/protetris/Piece.java
+++ app/src/main/java/com/example/protetris/Piece.java
@@ -1,4 +1,43 @@
 package com.example.protetris;
 
+import android.graphics.Point;
+
 public class Piece {
+
+    //Constants
+    public static final int S = 3; //Size piece
+
+    //Variables
+    public static boolean notVisible = true; //Shadow piece
+    public int[][] iBoard; //Board of shadow pieces.
+    public int[][] nBoard; //Normal Board
+    public Point iPos; //Position of shadow pieces.
+    private Point nPos; //Position on Board
+
+    //Methods
+
+    //Return the x coordinate of shadow pieces
+    public int getiPosX() {
+        return iPos.x;
+    }
+
+    //Return the y coordinate of shadow pieces
+    public int getiPosY() {
+        return iPos.y;
+
+    }
+
+    public Piece(int x, int y){
+        nBoard = new int[S][S];
+        iBoard = new int[S][S];
+
+    }
+
+    private void initPiece(int pieceSize){
+        for (int column = 0; column < pieceSize; column++) {
+            for (int row = 0; row < pieceSize; row++) {
+
+            }
+        }
+    }
 }
