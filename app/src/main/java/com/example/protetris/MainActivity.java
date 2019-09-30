0037ab43f207a2c65d080ebc8a718e0e2752e46f
Jose Luis <jlsierrabenito@gmail.com>
2019-09-30 09:14:48 +0200

    Cambios varios

--- app/src/main/java/com/example/protetris/MainActivity.java
+++ app/src/main/java/com/example/protetris/MainActivity.java
@@ -29,4 +29,9 @@ public class MainActivity extends AppCompatActivity {
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
     }
+
+    public void enterControls(View view) {
+        Intent intent = new Intent(this, Controls.class);
+        startActivity(intent);
+    }
 }
