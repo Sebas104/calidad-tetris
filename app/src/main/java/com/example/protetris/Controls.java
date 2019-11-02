3decd0209ea92cf19441b7e7905c03b6999559eb
Marcos <marcosrm99@gmail.com>
2019-11-02 16:14:48 +0100

    Agregado cambios visuales del controls, main colors y next piece

--- app/src/main/java/com/example/protetris/Controls.java
+++ app/src/main/java/com/example/protetris/Controls.java
@@ -2,7 +2,9 @@ package com.example.protetris;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Controls extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class Controls extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_controls);
+    }
+    public void startGame(View view) {
+        Intent intent = new Intent(this,MainColor.class);
+        startActivity(intent);
     }
 }
